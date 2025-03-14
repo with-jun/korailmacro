30c91a15462e1285df064e26bda3a36de3c06017
with-jun <jun9358@gmail.com>
2025-03-14 15:25:49 +0900

    feat: 신규 코레일 페이지 대응

--- content.js
+++ content.js
@@ -57,102 +57,60 @@ function bypassCapcha() {
 	});
 }
 
-var dsturl1 = "https://www.letskorail.com/ebizprd/EbizPrdTicketPr21111_i1.do";
-var dsturl2 = "https://www.letskorail.com/ebizprd/EbizPrdTicketpr21100W_pr21110.do";
-if (document.URL.substring(0, dsturl1.length) == dsturl1 ||
-	document.URL.substring(0, dsturl2.length) == dsturl2) {
+var dsturl1 = "https://www.korail.com/ticket/search/list";
+if (document.URL.substring(0, dsturl1.length) == dsturl1) {
 
-	$(document).ready(function() {
+	var bootstrap = function() {
+		var hasLoaded = $('.tckWrap .tckList .tck_inner').length > 0;
+		if (!hasLoaded) {
+			setTimeout(bootstrap, 100);
+			return;
+		}
+
 		bypassCapcha();
 		injectJs(chrome.extension.getURL('inject.js'));
 
 		var coachSelected = JSON.parse(localStorage.getItem('coachSelected'));
-		var firstSelected = JSON.parse(localStorage.getItem('firstSelected'));
 		if (coachSelected == null) coachSelected = [];
-		if (firstSelected == null) firstSelected = [];
-		console.log("coach:" + coachSelected);
-		console.log("first:" + firstSelected);
-
-		if (localStorage.getItem('macro') == "true") {
-			$(".btn_inq").append('<a href="#" onclick="macrostop();" style="font-size:15px; margin-left:5px;"><img src="' + chrome.extension.getURL('btn_stop.png') + '"></a>');
-			
-		} else {
-			$(".btn_inq").append('<a href="#" onclick="macro();" style="font-size:15px; margin-left:5px;"><img src="' + chrome.extension.getURL('btn_start.png') + '"></a>');
-		}
 
 		// 테이블에 "매크로" 버튼을 삽입한다.
-		if ($("#divResult").length != 0) {
-			var rows = $('#divResult > table.tbl_h tr');
-			for (i = 1; i < rows.length; i++) {
-				var columns = $(rows[i]).children('td');
-				var first = $(columns[4]);
-				var coach = $(columns[5]);
-				if (coach.children().length > 0) {
-					coach.append($("<p class='p5'></p>"));
-					var checkbox = $("<label></label>").html('<input type="checkbox" name="checkbox" class="coachMacro" value="' + i + '">매크로');
-					checkbox.children('input').prop('checked', coachSelected.indexOf(i+"") > -1);
-					coach.append(checkbox);
-				}
-				if (first.children().length > 0) {
-					first.append($("<p class='p5'></p>"));
-					var checkbox = $("<label></label>").html('<input type="checkbox" name="checkbox" class="firstMacro" value="' + i + '">매크로');
-					checkbox.children('input').prop('checked', firstSelected.indexOf(i+"") > -1);
-					first.append(checkbox);
-				}
+		if ($(".tckWrap").length != 0) {
+			var rows = $('.tckWrap .tckList .tck_inner');
+			for (i = 0; i < rows.length; i++) {
+				var trainNum = rows[i].querySelector('.num').textContent.trim();
+				var checkbox = $('<div class="fl-l search-option-bar__check-wrap"></div>').html('' +
+					'<input type="checkbox" id="coachMacro[' + trainNum + ']" name="checkbox" class="coachMacro" value="' + trainNum + '">' +
+					'<label for="coachMacro[' + trainNum + ']" class="search-option-bar__checkbox-rt">매크로</label>' +
+					'');
+				checkbox.children('input').prop('checked', coachSelected.indexOf(trainNum) > -1);
+				$(rows[i]).prepend(checkbox);
 			}
 		}
 
 		if (localStorage.getItem('macro') == "true") {
-
-			// Restore preferences
-			$("#peop01").val(localStorage.getItem('peop01'));
-			$("#peop02").val(localStorage.getItem('peop02'));
-			$("#peop03").val(localStorage.getItem('peop03'));
-			$("#obs13").val(localStorage.getItem('obs13'));
-			$("#obs46").val(localStorage.getItem('obs46'));
-			$("#seat01").val(localStorage.getItem('seat01'));
-			$("#seat02").val(localStorage.getItem('seat02'));
-			$("#seat03").val(localStorage.getItem('seat03'));
-
-			if ($("#divResult").length != 0) {
-				var rows = $('#divResult > table.tbl_h tr');
+			if ($(".tckWrap").length != 0) {
+				var rows = $('.tckWrap .tckList .tck_inner');
 
 				var succeed = false;
-				for (i = 1; i < rows.length; i++) {
-					var columns = $(rows[i]).children('td');
+				for (i = 0; i < rows.length; i++) {
+					var trainNum = rows[i].querySelector('.num').textContent.trim();
+					var coach = $(rows[i]).children('div')[2];
 
-					var first = $(columns[4]);
-					var coach = $(columns[5]);
-
-					if (coachSelected.indexOf(i+"") > -1) {
-						var coachSpecials = coach.children("a");
-						if (coachSpecials.length != 0) {
-							for (j = 0; j < coachSpecials.length; j++) {
-								img = $(coachSpecials[j]).children('img');
-								src = $(img).attr('src');
-								if (src == "/docs/2007/img/common/icon_apm_bl.gif" || src == "/docs/2007/img/common/icon_apm_rd.gif") {
-									redirectPage($(coachSpecials[j]).attr('href'));
-									succeed = true;
-									break;
+					if (coachSelected.indexOf(trainNum) > -1) {
+						if (!coach.classList.contains('sold_out')) {
+							coach.querySelector('a').click();
+							var tryClickReservBtn = function () {
+								var $btn = $('.ticket_reserv_wrap .reservbtn');
+								if ($btn.length != 0) {
+									$btn.click();
+									return;
 								}
-							}
-							if (succeed == true) break;
-						}
-					}
 
-					if (firstSelected.indexOf(i+"") > -1) {
-						var firstSpecials = first.children("a");
-						if (firstSpecials.length != 0) {
-							for (j = 0; j < firstSpecials.length; j++) {
-								img = $(firstSpecials[j]).children('img');
-								src = $(img).attr('src');
-								if (src == "/docs/2007/img/common/icon_apm_bl.gif" || src == "/docs/2007/img/common/icon_apm_rd.gif") {
-									redirectPage($(coachSpecials[j]).attr('href'));
-									succeed = true;
-									break;
-								}
-							}
-							if (succeed == true) break;
+								setTimeout(tryClickReservBtn, 100);
+							};
+							tryClickReservBtn();
+							succeed = true;
+							break;
 						}
 					}
 				}
@@ -160,20 +118,11 @@ if (document.URL.substring(0, dsturl1.length) == dsturl1 ||
 				if (succeed == true) {
 					localStorage.removeItem('macro');
 					localStorage.removeItem('coachSelected');
-					localStorage.removeItem('firstSelected');
-					localStorage.removeItem('peop01');
-					localStorage.removeItem('peop02');
-					localStorage.removeItem('peop03');
-					localStorage.removeItem('obs13');
-					localStorage.removeItem('obs46');
-					localStorage.removeItem('seat01');
-					localStorage.removeItem('seat02');
-					localStorage.removeItem('seat03');
 					chrome.extension.sendMessage({type: 'playSound'}, function(data) { });
 				} else {
 					// 모두 실패한 경우
-					setTimeout(function() { 
-					location.href = "javascript:inqSchedule();";
+					setTimeout(function() {
+						window.location.reload();
 					}, 500);
 				}
 			} else {
@@ -181,6 +130,15 @@ if (document.URL.substring(0, dsturl1.length) == dsturl1 ||
 				history.go(-1);
 			}
 		}
+	}
+	$(document).ready(function () {
+		if (localStorage.getItem('macro') == "true") {
+			$(".search-option-bar__wrap").append('<button type="button" onclick="macrostop();" style="font-size:15px; margin-left:5px;"><img src="' + chrome.extension.getURL('btn_stop.png') + '"></button>');
+		} else {
+			$(".search-option-bar__wrap").append('<button type="button" onclick="macro();" style="font-size:15px; margin-left:5px;"><img src="' + chrome.extension.getURL('btn_start.png') + '"></button>');
+		}
+
+		bootstrap();
 	});
 
 }
